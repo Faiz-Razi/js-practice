2c9e061ad47a29da2dd0a0a8eed9a2adc8d58006
Faiz <54997667+Faiz-Razi@users.noreply.github.com>
2025-07-29 14:49:12 +0000

    data types conversion

---
+++ 1-basics/3-conversion-operations.js
@@ -0,0 +1,20 @@
+// let score = "33abc" // NaN
+// let score = undefined // NaN
+// let score = null // 0
+// let score = true // 1
+// let score = false // 0
+// let score = 'faiz' // NaN
+
+// console.log(typeof score)
+// console.log(typeof (score)) // also correct
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let isPresent = 1
+
+console.log(isPresent)
+
+let isPresentBoolean = Boolean(isPresent)
+console.log(isPresentBoolean)
