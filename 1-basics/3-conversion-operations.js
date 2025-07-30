8cb023d08c28348af813dd6918fa077cfd105a8a
Faiz <54997667+Faiz-Razi@users.noreply.github.com>
2025-07-30 13:33:58 +0000

    comparision

--- 1-basics/3-conversion-operations.js
+++ 1-basics/3-conversion-operations.js
@@ -12,9 +12,57 @@
 // console.log(typeof valueInNumber)
 // console.log(valueInNumber)
 
-let isPresent = 1
+// let isPresent = 1
 
-console.log(isPresent)
+// console.log(isPresent)
 
-let isPresentBoolean = Boolean(isPresent)
-console.log(isPresentBoolean)
+// let isPresentBoolean = Boolean(isPresent)
+// console.log(isPresentBoolean)
+
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// Operations
+
+let value = 3
+
+let negValue = -value
+
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+// let str1 = 'hello'
+// let str2 = 'Faiz'
+
+// let str3 = str1 + str2
+
+// console.log(str3)
+
+// console.log( "1" + 2 )
+// console.log( 1 + "2" )
+// console.log( "1" + "2" )
+// console.log( "1" + 2 + 2 )
+// console.log( 1 + 2 + "2" )
+// console.log( (3 + 4) * 5 % 3 ) // don't do this
+
+// console.log( + true ) // don't do this
+// console.log( + '' ) // don't do this
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2 + 2 // all will be 4
+// console.log(num3)
+
+let gameCounter = 100
+++gameCounter; // study prefix postfix
+console.log(gameCounter)
+
+
